465dbe8d73040fe354ca0eaa36d5146f7e71a703
Yash Rathore <yrathore@amd.com>
2025-12-30 19:12:08 +0530

    Update generate_graph.py

--- github-actions/generate_graph.py
+++ github-actions/generate_graph.py
@@ -1,3 +1,13 @@
+# Copyright 2025 Advanced Micro Devices, Inc.
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+'''
+Generates/Updates the graph for daily passing status tracking
+'''
+
 import pandas as pd
 from pathlib import Path
 import plotly.graph_objects as go
